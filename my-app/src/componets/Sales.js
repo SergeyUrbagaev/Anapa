c4c0b6a514e48e6389c23da3eaac8fb81dde6f76
Sergey <urbagayev@mail.ru>
2024-12-28 17:43:26 +0300

    28.12

--- my-app/src/componets/Sales.js
+++ my-app/src/componets/Sales.js
@@ -6,26 +6,30 @@ export default function Sales() {
 			<h1 className="last-news">Последние новости</h1>
 			<div className="block-news">
 				<div className="conteiner">
-					<div className="conteiner-img"></div>
-					<div className="text-news">Скидки на</div>
+					<div className="conteiner-img" style={{ backgroundImage: "url('../img/sales2.jpeg')" }}></div>
+					<h1 className="main-news">Скидки на</h1>
+					<p>Планируйте свой отдых заранее и получите скидку 20%!</p>
 					<button>Узнать</button>
 				</div>
 				<div className="conteiner">
 					<div className="conteiner-img"></div>
-					<div className="text-news">Летние предложения для вас!</div>
+					<h1 className="main-news">Летние предложения для вас!</h1>
+					<p>Не упустите шанс отдохнуть на море по суперценам!</p>
 					<button>Узнать</button>
 				</div>
 				<div className="conteiner">
 					<div className="conteiner-img"></div>
-					<div className="text-news">Специальные предложения для семей!</div>
+					<h1 className="main-news">Специальные предложения для семей!</h1>
+					<p>Семейные пакеты с дополнительными бонусами для детей!</p>
 					<button>Узнать</button>
 				</div>
 				<div className="conteiner">
 					<div className="conteiner-img"></div>
-					<div className="text-news">Новые удобства в квартире!</div>
+					<h1 className="main-news">Новые удобства в квартире!</h1>
+					<p>Теперь у нас есть бассейн и зона для барбекю!</p>
 					<button>Узнать</button>
 				</div>
 			</div>
-		</div>
+		</div >
 	)
 }
