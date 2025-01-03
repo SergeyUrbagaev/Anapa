18b3823138a3abb111b08da328994979e596f90c
SergeyUrbagaev <urbagayev91@mail.ru>
2025-01-03 10:41:51 +0300

    03.12.25

--- my-app/src/componets/Sales.js
+++ my-app/src/componets/Sales.js
@@ -1,35 +1,40 @@
 import "../style/sales.css"
 
 export default function Sales() {
+	const backgroundImage1 = `url(${require('../img/sales1.jpeg')})`
+	const backgroundImage2 = `url(${require('../img/sales2.jpeg')})`
+	const backgroundImage3 = `url(${require('../img/sales3.jpg')})`
+	const backgroundImage4 = `url(${require('../img/sales4.jpeg')})`
 	return (
 		<div className="wraper-sales">
 			<h1 className="last-news">Последние новости</h1>
 			<div className="block-news">
 				<div className="conteiner">
-					<div className="conteiner-img" style={{ backgroundImage: "url('../img/sales2.jpeg')" }}></div>
-					<h1 className="main-news">Скидки на</h1>
-					<p>Планируйте свой отдых заранее и получите скидку 20%!</p>
-					<button>Узнать</button>
+					<div className="conteiner-img" style={{ backgroundImage: backgroundImage1 }}></div>
+					<h3 className="main-news">Скидки на</h3>
+					<p className="text-news">Планируйте свой отдых заранее и получите скидку 20%!</p>
+					<button className="butt-learn">Узнать</button>
 				</div>
 				<div className="conteiner">
-					<div className="conteiner-img"></div>
-					<h1 className="main-news">Летние предложения для вас!</h1>
-					<p>Не упустите шанс отдохнуть на море по суперценам!</p>
-					<button>Узнать</button>
+					<div className="conteiner-img" style={{ backgroundImage: backgroundImage2 }} alt={"2"}> </div>
+					<h3 className="main-news">Летние предложения для вас!</h3>
+					<p className="text-news">Не упустите шанс отдохнуть на море по суперценам!</p>
+					<button className="butt-learn">Узнать</button>
 				</div>
 				<div className="conteiner">
-					<div className="conteiner-img"></div>
-					<h1 className="main-news">Специальные предложения для семей!</h1>
-					<p>Семейные пакеты с дополнительными бонусами для детей!</p>
-					<button>Узнать</button>
+					<div className="conteiner-img" style={{ backgroundImage: backgroundImage3 }}></div>
+					<h3 className="main-news">Специальные предложения для семей!</h3>
+					<p className="text-news">Семейные пакеты с дополнительными бонусами для детей!</p>
+					<button className="butt-learn">Узнать</button>
 				</div>
 				<div className="conteiner">
-					<div className="conteiner-img"></div>
-					<h1 className="main-news">Новые удобства в квартире!</h1>
-					<p>Теперь у нас есть бассейн и зона для барбекю!</p>
-					<button>Узнать</button>
+					<div className="conteiner-img" style={{ backgroundImage: backgroundImage4 }}></div>
+					<h3 className="main-news">Новые удобства в квартире!</h3>
+					<p className="text-news">Теперь у нас есть бассейн и зона для барбекю!</p>
+					<button className="butt-learn">Узнать</button>
 				</div>
 			</div>
 		</div >
+
 	)
 }
