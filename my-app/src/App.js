f5541f3e28222d40aadaf082e1f0cb248251010b
SergeyUrbagaev <urbagayev91@mail.ru>
2025-01-09 00:35:15 +0300

    08.01.25

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,20 @@ import "./index.css";
 import Home from "./componets/Home";
 import Sales from "./componets/Sales";
 import Gallery from "./componets/Gallery";
+
 export default function App() {
+	const arrGalerey = [
+		'../img/sea.jpg',
+		'../img/sales2.jpeg',
+		'../img/sales3.jpg',
+		'../img/sales4.jpeg',
+	]
 	return (
 		<div>
 			<Navbar />
 			<Home />
 			<Sales />
-			<Gallery />
+			<Gallery photos={arrGalerey} />
 
 		</div>
 	);
--- my-app/src/componets/Gallery.jsx
+++ my-app/src/componets/Gallery.jsx
@@ -1,44 +1,37 @@
 import React, { useState } from 'react'
+
 import "../style/gallery.css";
 
-const images = [
-    { id: 1, src: '../img/sea.jpg', alt: 'sales1' },
-    { id: 2, src: '../img/sales2.jpeg', alt: 'sales2' },
-    { id: 3, src: '../img/sales3.jpg', alt: 'sales3' },
-    { id: 4, src: '../img/sales4.jpeg', alt: 'sales4' },
-]
-console.log(images[0]);
 
-const Gallery = () => {
-    const [selectedImage, setSelectedImage] = useState(null);
 
-    const openModal = (image) => {
-        setSelectedImage(image);
-    };
+const Gallery = ({ photos }) => {
+	const [currentIndex, setCurrentIndex] = useState(0);
+	const goToNext = () => {
+		setCurrentIndex((prevIndex) => (prevIndex + 1) % photos.length)
+	}
+	const goToPrev = () => {
+		setCurrentIndex((prevIndex) => (prevIndex - 1 + photos.length) % photos.length);
+	};
+	return (
+		<div className='gallery'>
+			<button
+				onClick={goToPrev}
+				className='but-prev'
+			>{"Предыдущая"}</button>
+			<div className='photoCont'>
+				<img
+					src={photos[currentIndex]}
+					className='photo'
+					alt={`Photo ${currentIndex}`}
+				/>
+			</div>
+			<button
+				onClick={goToNext}
+				className='but-prev'
+			>{"Следующая"}</button>
 
-    const closeModal = () => {
-        setSelectedImage(null);
-    };
 
-    return (
-        <div className="gallery">
-            {images.map((image) => (
-                <img
-                    key={image.id}
-                    src={image.src}
-                    alt={image.alt}
-                    className="gallery-image"
-                    onClick={() => openModal(image.src)}
-                />
-            ))}
-
-            {selectedImage && (
-                <div className="modal" onClick={closeModal}>
-                    <img src={selectedImage} alt="Selected" className="modal-image" />
-                </div>
-            )}
-        </div>
-    );
+		</div>)
 };
 
 export default Gallery;
